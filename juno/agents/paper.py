b87ec4ec1f032e7b207d6dfb027243336347e27c
discosultan <jvarus@blockport.io>
2019-04-19 16:28:52 +0200

    Sync

---
+++ juno/agents/paper.py
@@ -0,0 +1,127 @@
+import logging
+from decimal import Decimal
+from typing import Any, Dict, Optional
+
+from juno import Candle
+from juno.components import Informant, Orderbook
+from juno.math import adjust_size
+from juno.strategies import new_strategy
+
+from .agent import Agent
+from .summary import Position, TradingSummary
+
+_log = logging.getLogger(__name__)
+
+
+class Paper(Agent):
+
+    required_components = ['informant', 'orderbook']
+    open_position: Optional[Position]
+
+    async def run(self, exchange: str, symbol: str, interval: int, start: int, end: int,
+                  quote: Decimal, strategy_config: Dict[str, Any],
+                  restart_on_missed_candle: bool = True) -> TradingSummary:
+        assert end > start
+        assert quote > 0
+
+        self.exchange = exchange
+        self.symbol = symbol
+        self.quote = quote
+
+        informant: Informant = self.components['informant']
+        self.orderbook: Orderbook = self.components['orderbook']
+
+        self.fees = informant.get_fees(exchange)
+        _log.info(f'Fees: {self.fees}')
+
+        self.symbol_info = informant.get_symbol_info(exchange, symbol)
+        _log.info(f'Symbol info: {self.symbol_info}')
+
+        self.summary = TradingSummary(exchange, symbol, interval, start, end, quote, self.fees,
+                                      self.symbol_info)
+        self.open_position = None
+        restart_count = 0
+
+        while True:
+            last_candle = None
+            restart = False
+
+            strategy = new_strategy(strategy_config)
+
+            if restart_count == 0:
+                # Adjust start to accommodate for the required history before a strategy becomes
+                # effective. Only do it on first run because subsequent runs mean missed candles
+                # and we don't want to fetch passed a missed candle.
+                _log.info(f'fetching {strategy.req_history} candles before start time to warm-up '
+                          'strategy')
+                start -= strategy.req_history * interval
+
+            async for candle, primary in informant.stream_candles(
+                    exchange=exchange,
+                    symbol=symbol,
+                    interval=interval,
+                    start=start,
+                    end=end):
+                if not primary:
+                    continue
+
+                self.summary.append_candle(candle)
+
+                # Check if we have missed a candle.
+                if last_candle and candle.time - last_candle.time >= interval * 2:
+                    _log.warning(f'missed candle(s); last candle {last_candle}; current candle '
+                                 f'{candle}')
+                    if restart_on_missed_candle:
+                        _log.info('restarting strategy')
+                        start = candle.time
+                        restart = True
+                        restart_count += 1
+                        break
+
+                last_candle = candle
+                advice = strategy.update(candle)
+
+                if not self.open_position and advice == 1:
+                    if not self._try_open_position(candle):
+                        _log.warning(f'quote balance too low to open a position; stopping')
+                        break
+                elif self.open_position and advice == -1:
+                    self._close_position(candle)
+
+            if not restart:
+                break
+
+        if last_candle is not None and self.open_position:
+            self._close_position(last_candle)
+
+        return self.summary
+
+    def _try_open_position(self, candle: Candle) -> bool:
+        size = self.orderbook.find_market_order_buy_size(self.exchange, self.symbol, self.quote,
+                                                         self.symbol_info.size_step)
+
+        # size = self.quote / candle.close
+        # size = adjust_size(size, self.symbol_info.min_size, self.symbol_info.max_size,
+        #                    self.symbol_info.size_step)
+
+        if size == 0:
+            return False
+
+        self.open_position = Position(candle.time, size, candle.close, size * self.fees.taker)
+        self.quote -= size * candle.close
+
+        return True
+
+    def _close_position(self, candle: Candle) -> None:
+        assert self.open_position
+
+        base = self.open_position.base_size - self.open_position.base_fee
+        size = self.orderbook.find_market_order_sell_size(self.exchange, self.symbol, base,
+                                                          self.symbol_info.size_step)
+        quote = size * candle.close
+        fees = quote * self.fees.taker
+
+        self.open_position.close(candle.time, size, candle.close, fees)
+        self.summary.append_position(self.open_position)
+        self.open_position = None
+        self.quote = quote - fees
--- tests/test_agents.py
+++ tests/test_agents.py
@@ -3,8 +3,9 @@ from decimal import Decimal
 import pytest
 
 from juno import Candle, Fees, SymbolInfo
-from juno.agents import Agent, Backtest, list_required_component_names
+from juno.agents import Agent, Backtest, Paper, list_required_component_names
 from juno.agents.summary import Position
+from juno.components import Orderbook
 from juno.time import HOUR_MS
 
 from .utils import load_json_file
@@ -26,6 +27,14 @@ class FakeInformant:
     async def stream_candles(self, exchange, symbol, interval, start, end):
         for candle in self.candles:
             yield candle, True
+
+
+class FakeOrderbook(Orderbook):
+
+    def __init__(self, orderbooks):
+        self._orderbooks = orderbooks
+
+    # Uses size / price calculation from original orderbook impl.
 
 
 async def test_backtest(loop):
@@ -65,19 +74,20 @@ async def test_backtest(loop):
     async with Backtest(components={'informant': informant}, agent_config=agent_config) as agent:
         res = await agent.start()
 
-        assert res.profit == -50
-        assert res.potential_hodl_profit == 100
-        assert res.duration == 6
-        assert res.yearly_roi == -2629746000
-        assert res.max_drawdown == Decimal('0.75')
-        assert res.mean_drawdown == Decimal('0.25')
-        assert res.mean_position_profit == -25
-        assert res.mean_position_duration == 1
-        assert res.start == 0
-        assert res.end == 6
+    assert res.profit == -50
+    assert res.potential_hodl_profit == 100
+    assert res.duration == 6
+    assert res.yearly_roi == -2629746000
+    assert res.max_drawdown == Decimal('0.75')
+    assert res.mean_drawdown == Decimal('0.25')
+    assert res.mean_position_profit == -25
+    assert res.mean_position_duration == 1
+    assert res.start == 0
+    assert res.end == 6
 
 
-# Was failing as quote was incorrectly calculated after closing a position.
+# 1. was failing as quote was incorrectly calculated after closing a position.
+# 2. was failing as `juno.math.adjust_size` was rounding closest and not down.
 @pytest.mark.parametrize('scenario_nr', [1, 2])
 async def test_backtest_scenarios(loop, scenario_nr):
     path = f'/data/backtest_scenario{scenario_nr}_candles.json'
@@ -113,6 +123,58 @@ async def test_backtest_scenarios(loop, scenario_nr):
 
     async with Backtest(components={'informant': informant}, agent_config=agent_config) as agent:
         await agent.start()
+
+
+async def test_paper(loop):
+    informant = FakeInformant(
+        fees=Fees(Decimal(0), Decimal(0)),
+        symbol_info=SymbolInfo(
+            min_size=Decimal(1), max_size=Decimal(10000), size_step=Decimal(1),
+            min_price=Decimal(1), max_price=Decimal(10000), price_step=Decimal(1)),
+        candles=[
+            Candle(0, Decimal(1), Decimal(1), Decimal(1), Decimal(5), Decimal(1)),
+            Candle(1, Decimal(1), Decimal(1), Decimal(1), Decimal(10), Decimal(1)),
+            # Long. Size 10.
+            Candle(2, Decimal(1), Decimal(1), Decimal(1), Decimal(30), Decimal(1)),
+            Candle(3, Decimal(1), Decimal(1), Decimal(1), Decimal(20), Decimal(1)),
+            # Short.
+            Candle(4, Decimal(1), Decimal(1), Decimal(1), Decimal(40), Decimal(1)),
+            # Long. Size 5.
+            Candle(5, Decimal(1), Decimal(1), Decimal(1), Decimal(10), Decimal(1))
+        ])
+    orderbook = FakeOrderbook({
+        'dummy': {
+            'eth-btc': {
+                'asks': {
+                    Decimal(1): Decimal(2)
+                },
+                'bids': {
+                }
+            }
+        }
+    })
+    agent_config = {
+        'exchange': 'dummy',
+        'symbol': 'eth-btc',
+        'interval': 1,
+        'start': 0,
+        'end': 6,
+        'quote': Decimal(100),
+        'strategy_config': {
+            'name': 'emaemacx',
+            'short_period': 1,
+            'long_period': 2,
+            'neg_threshold': Decimal(-1),
+            'pos_threshold': Decimal(1),
+            'persistence': 0
+        }
+    }
+
+    async with Paper(components={'informant': informant, 'orderbook': orderbook},
+                     agent_config=agent_config) as agent:
+        res = await agent.start()
+
+    assert res
 
 
 def test_position():
