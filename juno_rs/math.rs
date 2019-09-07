8ca6fff247d345703779b8ce8ab8ba6451a41ff7
discosultan <jvarus@blockport.io>
2019-09-07 15:42:37 +0200

    Add Rust rounding test

--- juno_rs/math.rs
+++ juno_rs/math.rs
@@ -4,3 +4,13 @@ pub fn round_half_up(value: f64, precision: u32) -> f64 {
     let factor = 10_u32.pow(precision) as f64;
     (value * factor).round() / factor
 }
+
+#[cfg(test)]
+mod tests {
+    use super::round_half_up;
+
+    #[test]
+    fn test_round_half_up() {
+        assert_eq!(round_half_up(1.234, 2), 1.23);
+    }
+}
