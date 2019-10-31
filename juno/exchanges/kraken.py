f3dd94cad04d8692b7a30be15a9689df92a87bd0
Jaanus Varus <jaanusvarus@gmail.com>
2019-10-31 16:45:19 +0100

    Sync

--- juno/exchanges/kraken.py
+++ juno/exchanges/kraken.py
@@ -1,27 +1,36 @@
 from __future__ import annotations
 
+import asyncio
 import base64
 import hashlib
 import hmac
 import logging
 import urllib.parse
+from collections import defaultdict
 from contextlib import asynccontextmanager
 from decimal import Decimal
-from typing import Any, AsyncIterable, AsyncIterator, Dict, Optional, Union
+from typing import (
+    Any, AsyncContextManager, AsyncIterable, AsyncIterator, Dict, List, Optional, Union
+)
 
-import simplejson as json
-
+import juno.json as json
 from juno import (
     Balance, CancelOrderResult, Candle, DepthSnapshot, DepthUpdate, Fees, Filters, OrderResult,
     OrderType, OrderUpdate, Side, SymbolsInfo, TimeInForce
 )
-from juno.http import ClientSession
+from juno.asyncio import Event, cancel, cancelable
+from juno.http import ClientSession, ClientWebSocketResponse
 from juno.time import time_ms
 from juno.typing import ExcType, ExcValue, Traceback
 
 from .exchange import Exchange
 
-_BASE_URL = 'https://api.kraken.com'
+_API_URL = 'https://api.kraken.com'
+
+# https://docs.kraken.com/websockets/
+# https://support.kraken.com/hc/en-us/articles/360022326871-Public-WebSockets-API-common-questions
+_PUBLIC_WS_URL = 'wss://ws.kraken.com'
+_PRIVATE_WS_URL = 'wss://ws-auth.kraken.com'
 
 _log = logging.getLogger(__name__)
 
@@ -33,11 +42,15 @@ class Kraken(Exchange):
         self._decoded_secret_key = base64.b64decode(secret_key)
 
     async def __aenter__(self) -> Kraken:
+        # TODO: concurrently
         self._session = ClientSession(raise_for_status=True)
         await self._session.__aenter__()
+        self._public_ws = KrakenTopic(_PUBLIC_WS_URL)
+        await self._public_ws.__aenter__()
         return self
 
     async def __aexit__(self, exc_type: ExcType, exc: ExcValue, tb: Traceback) -> None:
+        await self._public_ws.__aexit__(exc_type, exc, tb)
         await self._session.__aexit__(exc_type, exc, tb)
 
     async def get_symbols_info(self) -> SymbolsInfo:
@@ -84,7 +97,16 @@ class Kraken(Exchange):
     async def connect_stream_depth(
         self, symbol: str
     ) -> AsyncIterator[AsyncIterable[Union[DepthSnapshot, DepthUpdate]]]:
-        yield  # type: ignore
+        async def inner(ws: AsyncIterable[Any]) -> AsyncIterable[Any]:
+            async for msg in ws:
+                _log.critical(msg)
+                yield True
+
+        async with self._public_ws.subscribe([_symbol(symbol)], {
+            'name': 'book',
+            'depth': 10,
+        }) as subscription:
+            yield inner(subscription)
 
     @asynccontextmanager
     async def connect_stream_orders(self) -> AsyncIterator[AsyncIterable[OrderUpdate]]:
@@ -133,10 +155,97 @@ class Kraken(Exchange):
         self, method: str, url: str, data: Dict[str, Any], headers: Dict[str, str] = {}
     ):
         async with self._session.request(
-            method=method, url=_BASE_URL + url, data=data, headers=headers
+            method=method, url=_API_URL + url, data=data, headers=headers
         ) as res:
             result = await res.json(loads=json.loads)
             errors = result['error']
             if len(errors) > 0:
                 raise Exception(errors)
             return result
+
+
+class KrakenTopic:
+    def __init__(self, url: str) -> None:
+        self.url = url
+
+        self.session = ClientSession(raise_for_status=True)
+        self.ws_ctx: Optional[AsyncContextManager[ClientWebSocketResponse]] = None
+        self.ws: Optional[ClientWebSocketResponse] = None
+        self.ws_lock = asyncio.Lock()
+        self.process_task: Optional[asyncio.Task] = None
+
+        self.reqid = 1
+        self.subscriptions: Dict[int, Event[Event[Any]]] = defaultdict(
+            lambda: Event(autoclear=True)
+        )
+        self.channels: Dict[int, Event[Any]] = defaultdict(lambda: Event(autoclear=True))
+
+    async def __aenter__(self) -> KrakenTopic:
+        await self.session.__aenter__()
+        return self
+
+    async def __aexit__(self, exc_type: ExcType, exc: ExcValue, tb: Traceback) -> None:
+        await cancel(self.process_task)
+        if self.ws:
+            await self.ws.close()
+        if self.ws_ctx:
+            await self.ws_ctx.__aexit__(exc_type, exc, tb)
+        await self.session.__aexit__(exc_type, exc, tb)
+
+    @asynccontextmanager
+    async def subscribe(
+        self, pairs: List[str], subscription: Any
+    ) -> AsyncIterator[AsyncIterable[Any]]:
+        await self._ensure_connection()
+        assert self.ws
+
+        reqid = self.reqid
+        subscribed = self.subscriptions[reqid]
+        self.reqid += 1
+        await self.ws.send_json({
+            'event': 'subscribe',
+            'reqid': reqid,
+            'pair': pairs,
+            'subscription': subscription,
+        })
+
+        received = await subscribed.wait()
+
+        async def inner() -> AsyncIterable[Any]:
+            while True:
+                yield await received.wait()
+
+        try:
+            yield inner()
+        finally:
+            # TODO: unsubscribe
+            pass
+
+    async def _ensure_connection(self) -> None:
+        async with self.ws_lock:
+            if self.ws:
+                return
+
+            self.ws_ctx = self.session.ws_connect(self.url)
+            self.ws = await self.ws_ctx.__aenter__()
+
+            self.process_task = asyncio.create_task(cancelable(self._process()))
+
+    async def _process(self) -> None:
+        assert self.ws
+        async for msg in self.ws:
+            data = json.loads(msg.data)
+            _log.critical(data)
+            _log.critical(type(data))
+            if isinstance(data, dict):
+                if data['event'] == 'subscriptionStatus':
+                    subscribed = self.subscriptions[data['reqid']]
+                    received = self.channels[data['channelID']]
+                    subscribed.set(received)
+            else:  # List.
+                channel_id, data, type_, pair = data  # type: ignore
+                self.channels[channel_id].set(data)  # type: ignore
+
+
+def _symbol(symbol: str) -> str:
+    return symbol.replace('-', '/').upper()
