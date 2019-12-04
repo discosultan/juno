8d9ce82780cd7f2ce98f731b878b31d9956f8aa0
discosultan <jaanusvarus@gmail.com>
2019-12-04 09:28:52 +0100

    Add RUB to Binance base asset list

--- juno/exchanges/binance.py
+++ juno/exchanges/binance.py
@@ -639,7 +639,8 @@ def _from_symbol(symbol: str) -> str:
     # TODO: May be incorrect! We can't systematically know which part is base and which is quote
     # since there is no separator used. We simply map based on known base currencies.
     known_base_assets = [
-        'BNB', 'BTC', 'ETH', 'XRP', 'USDT', 'PAX', 'TUSD', 'USDC', 'USDS', 'TRX', 'BUSD', 'NGN'
+        'BNB', 'BTC', 'ETH', 'XRP', 'USDT', 'PAX', 'TUSD', 'USDC', 'USDS', 'TRX', 'BUSD', 'NGN',
+        'RUB'
     ]
     for known_base_asset in known_base_assets:
         if symbol.endswith(known_base_asset):
