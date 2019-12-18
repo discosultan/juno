8fce7aa132f0e47019eb6b76df96523debe1dc79
discosultan <jaanusvarus@gmail.com>
2019-12-18 21:57:09 +0100

    Add export analytics csv script

---
+++ scripts/export_analytics_csv.py
@@ -0,0 +1,140 @@
+import asyncio
+import csv
+import logging
+import os
+from decimal import Decimal
+from typing import Any, Dict, List
+
+from juno import Candle
+from juno.asyncio import list_async
+from juno.components import Chandler, Informant, Trades
+from juno.exchanges import Binance, Coinbase
+from juno.math import ceil_multiple, floor_multiple
+from juno.storages import SQLite
+from juno.strategies import MAMACX
+from juno.time import DAY_MS, HOUR_MS, datetime_utcfromtimestamp_ms, strptimestamp
+from juno.trading import Trader, TradingSummary
+from juno.utils import unpack_symbol
+
+SYMBOL = 'eth-btc'
+INTERVAL = HOUR_MS
+
+
+async def main():
+    sqlite = SQLite()
+    binance = Binance(
+        os.environ[f'JUNO__BINANCE__API_KEY'], os.environ[f'JUNO__BINANCE__SECRET_KEY']
+    )
+    coinbase = Coinbase(
+        os.environ[f'JUNO__COINBASE__API_KEY'], os.environ[f'JUNO__COINBASE__SECRET_KEY'],
+        os.environ[f'JUNO__COINBASE__PASSPHRASE']
+    )
+    trades = Trades(sqlite, [binance, coinbase])
+    chandler = Chandler(trades, sqlite, [binance, coinbase])
+    informant = Informant(sqlite, [binance, coinbase])
+    start = floor_multiple(strptimestamp('2019-01-01'), INTERVAL)
+    end = floor_multiple(strptimestamp('2019-12-01'), INTERVAL)
+    async with binance, coinbase, informant:
+        trader = Trader(
+            chandler=chandler,
+            informant=informant,
+            exchange='binance',
+            symbol=SYMBOL,
+            interval=INTERVAL,
+            start=start,
+            end=end,
+            quote=Decimal('1.0'),
+            new_strategy=lambda: MAMACX(3, 73, Decimal('-0.102'), Decimal('0.239'), 4, 'sma',
+                                        'smma'),
+            trailing_stop=Decimal('0.0827'),
+            missed_candle_policy='last'
+        )
+        await trader.run()
+
+        await asyncio.gather(
+            stream_and_export_daily_candles_as_csv(
+                chandler, trader.summary, 'coinbase', 'btc-eur'
+            ),
+            stream_and_export_daily_candles_as_csv(
+                chandler, trader.summary, 'binance', SYMBOL
+            ),
+            asyncio.get_running_loop().run_in_executor(
+                None, export_trading_summary_as_csv, trader.summary, SYMBOL
+            ),
+        )
+
+    logging.info('done')
+
+
+async def stream_and_export_daily_candles_as_csv(
+    chandler: Chandler, summary: TradingSummary, exchange: str, symbol: str
+) -> None:
+    candles = await list_async(chandler.stream_candles(
+        exchange,
+        symbol,
+        DAY_MS,
+        floor_multiple(summary.start, DAY_MS),
+        ceil_multiple(summary.end, DAY_MS)
+    ))
+    await asyncio.get_running_loop().run_in_executor(
+        None, export_daily_candles_as_csv, symbol, candles
+    )
+
+
+def export_daily_candles_as_csv(symbol: str, candles: List[Candle]) -> None:
+    with open(f'{symbol}-max.csv', 'w', newline='') as csvfile:
+        fieldnames = ['snapped_at', 'price', 'market_cap', 'total_volume']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for candle in candles:
+            writer.writerow(candle_row(candle))
+
+
+def candle_row(candle: Candle) -> Dict[str, Any]:
+    return {
+        'snapped_at': datetime_utcfromtimestamp_ms(candle.time).strftime(r'%Y-%m-%d 00:00:00 UTC'),
+        'price': str(candle.close),
+        'market_cap': '0.0',
+        'total_volume': '0.0'
+    }
+
+
+def export_trading_summary_as_csv(summary: TradingSummary, symbol: str) -> None:
+    base_asset, quote_asset = unpack_symbol(symbol)
+
+    with open('tradesheet.csv', 'w', newline='') as csvfile:
+        fieldnames = ['Date', 'Buy', 'Sell', 'Units', 'Value Per Unit']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        EUR = Decimal('3347.23')
+
+        writer.writeheader()
+        writer.writerow(
+            trade_row(summary.start, 'EUR', '', summary.quote * EUR, Decimal('1.0'))
+        )
+        writer.writerow(
+            trade_row(summary.start, quote_asset, 'EUR', summary.quote, Decimal('3347.23'))
+        )
+        for pos in summary.positions:
+            size = pos.fills.total_size - pos.fills.total_fee
+            price = pos.fills.total_quote / size
+            writer.writerow(trade_row(pos.time, base_asset, quote_asset, size, price))
+            size = pos.gain
+            price = (pos.fills.total_size - pos.fills.total_fee) / pos.gain
+            writer.writerow(trade_row(pos.closing_time, quote_asset, base_asset, size, price))
+
+
+def trade_row(
+    time: int, buy_asset: str, sell_asset: str, buy_size: Decimal, buy_price: Decimal
+) -> Dict[str, Any]:
+    return {
+        'Date': datetime_utcfromtimestamp_ms(time).strftime(r'%m/%d/%Y'),
+        'Buy': buy_asset.upper(),
+        'Sell': sell_asset.upper(),
+        'Units': str(buy_size),
+        'Value Per Unit': str(buy_price),
+    }
+
+
+asyncio.run(main())
