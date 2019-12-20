bcb75bed445ea7a54715d97a39ffae71914cd047
Jaanus Varus <jaanusvarus@gmail.com>
2019-12-20 15:52:46 +0100

    Cleanup

--- juno/tenacity.py
+++ juno/tenacity.py
@@ -1,7 +1,6 @@
 from time import time
 from typing import Any, Callable
 
-from tenacity import compat
 from tenacity.stop import stop_base
 
 
@@ -22,20 +21,9 @@ class stop_after_attempt_with_reset(stop_base):
         self.last_attempt_at = 0.0
         self.attempt_offset = 0
 
-    @compat.stop_dunder_call_accept_old_params
     def __call__(self, retry_state: Any) -> bool:
         now = self.get_time()
-        import logging
-        logging.critical(now)
-        logging.critical(self.last_attempt_at)
-        logging.critical(self.time_to_reset)
         if now - self.last_attempt_at >= self.time_to_reset:
             self.attempt_offset = retry_state.attempt_number - 1
         self.last_attempt_at = now
-
-        logging.critical('purse')
-
-        logging.critical(f'{retry_state.attempt_number=}')
-        logging.critical(f'{self.attempt_offset=}')
-        logging.critical(f'{self.max_attempt_number=}')
         return retry_state.attempt_number - self.attempt_offset >= self.max_attempt_number
