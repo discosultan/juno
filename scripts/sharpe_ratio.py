d8642c76c1a8c5c8ce8c833baa44565fe90425f2
Jaanus Varus <jaanusvarus@gmail.com>
2019-12-23 14:52:22 +0100

    Sync

---
+++ scripts/sharpe_ratio.py
@@ -0,0 +1,182 @@
+import asyncio
+import logging
+import operator
+from collections import defaultdict
+from decimal import Decimal
+from typing import Any, Dict, List, Tuple
+
+import numpy as np
+import pandas as pd
+
+from juno import Fill
+from juno.asyncio import list_async
+from juno.components import Chandler, Informant, Trades
+from juno.config import config_from_env, load_instance
+from juno.exchanges import Binance, Coinbase
+from juno.math import floor_multiple
+from juno.storages import SQLite
+from juno.strategies import MAMACX
+from juno.time import DAY_MS, HOUR_MS, strptimestamp
+from juno.trading import Trader
+from juno.utils import unpack_symbol
+
+SYMBOL = 'eth-btc'
+INTERVAL = HOUR_MS
+
+
+async def main() -> None:
+    sqlite = SQLite()
+    config = config_from_env()
+    binance = load_instance(Binance, config)
+    coinbase = load_instance(Coinbase, config)
+    chandler = Chandler(
+        Trades(sqlite, [binance, coinbase]),
+        sqlite,
+        [binance, coinbase]
+    )
+    informant = Informant(sqlite, [binance, coinbase])
+    start = floor_multiple(strptimestamp('2019-01-01'), INTERVAL)
+    end = floor_multiple(strptimestamp('2019-12-01'), INTERVAL)
+    quote_asset, base_asset = unpack_symbol(SYMBOL)
+    async with binance, coinbase, informant:
+        trader = Trader(
+            chandler=chandler,
+            informant=informant,
+            exchange='binance',
+            symbol=SYMBOL,
+            interval=INTERVAL,
+            start=start,
+            end=end,
+            quote=Decimal('1.0'),
+            new_strategy=lambda: MAMACX(3, 73, Decimal('-0.102'), Decimal('0.239'), 4, 'sma',
+                                        'smma'),
+            trailing_stop=Decimal('0.0827'),
+            missed_candle_policy='last'
+        )
+        await trader.run()
+
+        _, filters = informant.get_fees_filters('binance', SYMBOL)
+
+        start_day = floor_multiple(start, DAY_MS)
+        end_day = start_day + DAY_MS
+        trade_days = (end_day - start_day) / DAY_MS
+        # 1. Assumes symbol base is BTC.
+        # 2. We run it after trading step, because it might use the same candle configuration which
+        # we don't support processing concurrently.
+
+        btc_eur_daily, symbol_daily = await asyncio.gather(
+            # {c.time: c async for c in chandler.stream_candles(
+            #     'coinbase', 'btc-eur', DAY_MS, start_day, end_day)
+            #  },
+            # {c.time: c async for c in chandler.stream_candles(
+            #     'binance', SYMBOL, DAY_MS, start_day, end_day)
+            #  },
+            list_async(chandler.stream_candles('coinbase', 'btc-eur', DAY_MS, start_day, end_day)),
+            list_async(chandler.stream_candles('binance', SYMBOL, DAY_MS, start_day, end_day)),
+        )
+        assert len(btc_eur_daily) == len(symbol_daily)
+        market_data = {
+            'btc': {c.time: c.close for c in btc_eur_daily},
+            base_asset: {c.time: c.close for c in symbol_daily},
+        }
+
+        # eur_investment = trader.summary.quote * btc_eur_daily[0].close
+
+        trades: Dict[int, List[Tuple[str, Any, Decimal]]] = defaultdict(list)
+        for pos in trader.summary.positions:
+            time = floor_multiple(pos.time, DAY_MS)
+            day_trades = trades[time]
+            day_trades.append((quote_asset, operator.sub, Fill.total_quote(pos.fills)))
+            day_trades.append(
+                (base_asset, operator.add, Fill.total_size(pos.fills) - Fill.total_fee(pos.fills))
+            )
+
+        asset_holdings: Dict[str, Decimal] = defaultdict(lambda: Decimal('0.0'))
+        asset_holdings[quote_asset] = trader.summary.quote
+
+        asset_performance: Dict[int, Dict[str, Decimal]] = defaultdict(
+            lambda: defaultdict(lambda: Decimal('0.0'))
+        )
+        # portfolio_performance = {}
+
+        for time_day in range(start_day, end_day, DAY_MS):
+            # Update holdings.
+            # TODO: Improve naming.
+            day_trades2 = trades.get(time_day)
+            if day_trades2:
+                for asset, op, size in day_trades2:
+                    asset_holdings[asset] = op(asset_holdings[asset], size)
+
+            # Update asset performance.
+            asset_performance_day = asset_performance[time_day]
+            for asset in asset_performance_day.keys():
+                asset_eur_value = market_data[asset].get(time_day)
+                if asset_eur_value is not None:
+                    asset_performance_day[asset] = asset_holdings[asset] * asset_eur_value
+                else:  # Missing asset market data for the day.
+                    # TODO: What if previous day also missing?? Maybe better to fill missing
+                    # candles?? Remove assert above.
+                    asset_performance_day[asset] = asset_performance[time_day - DAY_MS][asset]
+
+        # Update portfolio performance.
+        portfolio_performance = pd.Series(
+            [float(sum(v for v in apd.values())) for apd in asset_performance.values()]
+        )
+        portfolio_a_returns = portfolio_performance.pct_change().dropna()
+        portfolio_g_returns = np.log(portfolio_a_returns + 1)
+        portfolio_neg_g_returns = portfolio_g_returns[portfolio_g_returns < 0].dropna()
+
+        benchmark_performance = pd.Series([float(c.close) for c in btc_eur_daily])
+        benchmark_a_returns = benchmark_performance.pct_change().dropna()
+        benchmark_g_returns = np.log(benchmark_a_returns + 1)
+        benchmark_neg_g_returns = benchmark_g_returns[benchmark_g_returns < 0].dropna()
+
+        # Compute benchmark statistics.
+        benchmark_total_return = (benchmark_performance[-1] / benchmark_performance[0]) - 1
+        benchmark_annualized_return = 365 * benchmark_g_returns.mean()
+        benchmark_annualized_volatility = np.sqrt(365) * benchmark_g_returns.std()
+        benchmark_annualized_downside_risk = np.sqrt(365) * benchmark_neg_g_returns.std()
+        benchmark_sharpe_ratio = benchmark_annualized_return / benchmark_annualized_volatility
+        benchmark_sortino_ratio = benchmark_annualized_return / benchmark_annualized_downside_risk
+        benchmark_cagr = (
+            (benchmark_performance[-1] / benchmark_performance[0]) **
+            (1 / (trade_days / 365))
+        ) - 1
+
+        # Compute portfolio statistics.
+        portfolio_total_return = (portfolio_performance[-1] / portfolio_performance[0]) - 1
+        portfolio_annualized_return = 365 * portfolio_g_returns.mean()
+        portfolio_annualized_volatility = np.sqrt(365) * portfolio_g_returns.std()
+        portfolio_annualized_downside_risk = np.sqrt(365) * portfolio_neg_g_returns.std()
+        portfolio_sharpe_ratio = portfolio_annualized_return / portfolio_annualized_volatility
+        portfolio_sortino_ratio = portfolio_annualized_return / portfolio_annualized_downside_risk
+        portfolio_cagr = (
+            (portfolio_performance[-1] / portfolio_performance[0]) **
+            (1 / (trade_days / 365))
+        ) - 1
+        # covariance_matrix = pd.concat(
+        #   [g_returns['Value'], benchmark_g_returns], axis=1
+        # ).dropna().cov()
+        # beta = covariance_matrix.iloc[0].iloc[1] / covariance_matrix.iloc[1].iloc[1]
+        # alpha = annualized_return - (beta * 365 * benchmark_g_returns.mean())
+
+        logging.info(f'{benchmark_total_return=}')
+        logging.info(f'{benchmark_annualized_return=}')
+        logging.info(f'{benchmark_annualized_volatility=}')
+        logging.info(f'{benchmark_annualized_downside_risk=}')
+        logging.info(f'{benchmark_sharpe_ratio=}')
+        logging.info(f'{benchmark_sortino_ratio=}')
+        logging.info(f'{benchmark_cagr=}')
+
+        logging.info(f'{portfolio_total_return=}')
+        logging.info(f'{portfolio_annualized_return=}')
+        logging.info(f'{portfolio_annualized_volatility=}')
+        logging.info(f'{portfolio_annualized_downside_risk=}')
+        logging.info(f'{portfolio_sharpe_ratio=}')
+        logging.info(f'{portfolio_sortino_ratio=}')
+        logging.info(f'{portfolio_cagr=}')
+
+    logging.info('done')
+
+
+asyncio.run(main())
