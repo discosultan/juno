6e8958dda26fd2638b30a81d93e9bed8657ecbab
discosultan <jaanusvarus@gmail.com>
2020-01-21 22:58:21 +0100

    Add MACD strategy

--- juno/strategies/__init__.py
+++ juno/strategies/__init__.py
@@ -1,8 +1,10 @@
+from .macd import MACD
 from .mamacx import MA, MAMACX
 from .strategy import Meta, Strategy
 
 __all__ = [
     'MA',
+    'MACD',
     'MAMACX',
     'Meta',
     'Strategy',
---
+++ juno/strategies/macd.py
@@ -0,0 +1,47 @@
+from juno import Advice, Candle, Trend, indicators, math
+from juno.utils import Persistence
+
+from .strategy import Meta, Strategy
+
+
+# Moving Average Convergence Divergence.
+class MACD(Strategy):
+    meta = Meta(
+        constraints={
+            'short_period': math.Constant(12),
+            'long_period': math.Constant(26),
+            'signal_period': math.Constant(9),
+            'persistence': math.Int(0, 10),
+        }
+    )
+
+    def __init__(
+        self,
+        short_period: int,
+        long_period: int,
+        signal_period: int,
+        persistence: int
+    ) -> None:
+        self.validate(short_period, long_period, signal_period, persistence)
+        self._macd = indicators.Macd(short_period, long_period, signal_period)
+        self._persistence = Persistence(level=persistence, allow_initial_trend=False)
+        self._t = 0
+        self._t1 = long_period - 1
+
+    @property
+    def req_history(self) -> int:
+        return self._t1
+
+    def update(self, candle: Candle) -> Advice:
+        self._macd.update(candle.close)
+
+        trend = Trend.UNKNOWN
+        if self._t == self._t1:
+            if self._macd.value > self._macd.signal:
+                trend = Trend.UP
+            else:
+                trend = Trend.DOWN
+
+        self._t = min(self._t + 1, self._t1)
+
+        return Strategy.advice(*self._persistence.update(trend))
