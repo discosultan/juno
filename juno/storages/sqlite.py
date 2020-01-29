3e30456763b58314654f65660fe76b45878ed5d5
Jaanus Varus <jaanusvarus@gmail.com>
2020-01-29 17:26:08 +0100

    Revert sqlite v

--- juno/storages/sqlite.py
+++ juno/storages/sqlite.py
@@ -19,7 +19,7 @@ from .storage import Storage
 _log = logging.getLogger(__name__)
 
 # Version should be incremented every time a storage schema changes.
-_VERSION = 'temp'
+_VERSION = '35'
 
 T = TypeVar('T')
 
