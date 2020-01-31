bd096e4fec971a1c6425d593f6b11bebb73f0642
discosultan <jaanusvarus@gmail.com>
2020-01-31 16:39:30 +0100

    Update chandler stream candles script

--- scripts/chandler_stream_candles.py
+++ scripts/chandler_stream_candles.py
@@ -1,21 +1,27 @@
 import asyncio
 import logging
 
-from juno import exchanges
+from juno import exchanges, time
 from juno.asyncio import enumerate_async
 from juno.components import Chandler, Trades
 from juno.config import from_env, init_instance
 from juno.math import floor_multiple
 from juno.storages import SQLite
-from juno.time import HOUR_MS, MIN_MS, time_ms
+from juno.time import time_ms
 
-EXCHANGE_TYPE = exchanges.Binance
-SYMBOL = 'eth-btc'
-INTERVAL = MIN_MS
+# EXCHANGE_TYPE = exchanges.Binance
+# SYMBOL = 'eth-btc'
+# INTERVAL = time.MIN_MS
+# # Should fetch 2 historical and rest future.
+# START = floor_multiple(time_ms(), INTERVAL) - 2 * INTERVAL
+# END = START + time.HOUR_MS
+
+EXCHANGE_TYPE = exchanges.Coinbase
+SYMBOL = 'btc-eur'
+INTERVAL = time.DAY_MS
 # Should fetch 2 historical and rest future.
-NOW = floor_multiple(time_ms(), MIN_MS)
-START = NOW - 2 * MIN_MS
-END = START + HOUR_MS
+START = time.strptimestamp('2018-01-01')
+END = time.strptimestamp('2020-01-01')
 
 
 async def main() -> None:
@@ -24,18 +30,19 @@ async def main() -> None:
     name = EXCHANGE_TYPE.__name__.lower()
     trades = Trades(sqlite, [client])
     chandler = Chandler(trades=trades, storage=sqlite, exchanges=[client])
+    now = floor_multiple(time_ms(), INTERVAL)
     async with client:
-        start = floor_multiple(time_ms(), MIN_MS) - 2 * MIN_MS
-
-        logging.info(f'start {start}')
+        logging.info(f'start {START}')
         async for i, candle in enumerate_async(chandler.stream_candles(
             name, SYMBOL, INTERVAL, START, END, closed=False
         )):
-            historical_or_future = 'future' if candle.time >= NOW else 'historical'
+            historical_or_future = 'future' if candle.time >= now else 'historical'
             logging.info(f'{historical_or_future} candle {i}: {candle}')
             assert candle.time == START + i * INTERVAL
-            if candle.time < NOW:
+            if candle.time < now:
                 assert candle.closed
+            else:
+                break
 
         logging.info('done')
 
