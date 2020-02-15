d8c7202070e2ee85ef37b4cc4027a3ae0f3ad272
discosultan <jaanusvarus@gmail.com>
2020-02-15 12:38:47 +0100

    Add script for optimization validation

---
+++ scripts/time_series_cross_validation.py
@@ -0,0 +1,99 @@
+import asyncio
+import logging
+from decimal import Decimal
+
+from juno import strategies, time
+from juno.components import Chandler, Historian, Informant, Prices, Trades
+from juno.config import from_env, init_instance
+from juno.exchanges import Binance, Coinbase
+from juno.math import floor_multiple
+from juno.optimization import Optimizer, Rust
+from juno.storages import SQLite
+from juno.trading import Trader, analyse_benchmark, analyse_portfolio
+from juno.utils import format_attrs_as_json, unpack_symbol
+
+SYMBOLS = 'eth-btc'
+INTERVAL = time.HOUR_MS
+TRAINING_VALIDATION_SPLIT = 0.80
+QUOTE = Decimal('1.0')
+STRATEGY_TYPE = strategies.MAMACX
+
+
+async def main() -> None:
+    sqlite = SQLite()
+    binance = init_instance(Binance, from_env())
+    coinbase = init_instance(Coinbase, from_env())
+    exchanges = [binance, coinbase]
+    exchange_name = Binance.__name__.lower()
+    trades = Trades(sqlite, exchanges)
+    chandler = Chandler(trades=trades, storage=sqlite, exchanges=exchanges)
+    historian = Historian(chandler=chandler, storage=sqlite, exchanges=exchanges)
+    informant = Informant(sqlite, exchanges)
+    prices = Prices(chandler)
+    trader = Trader(chandler, informant)
+    rust_solver = Rust()
+    optimizer = Optimizer(rust_solver, chandler, informant, prices, trader)
+    async with binance, coinbase, informant, rust_solver:
+        first_candle = await historian.find_first_candle(exchange_name, SYMBOL, INTERVAL)
+        training_start = floor_multiple(first_candle.time, INTERVAL)
+        validation_end = floor_multiple(time.time_ms(), INTERVAL)
+        validation_start = floor_multiple(
+            training_start + int((validation_end - training_start) * TRAINING_VALIDATION_SPLIT),
+            INTERVAL
+        )
+
+        optimization_summary = await optimizer.run(
+            exchange=exchange_name,
+            start=training_start,
+            end=validation_start,
+            quote=QUOTE,
+            strategy_type=STRATEGY_TYPE,
+            symbols=[SYMBOL],
+            intervals=[INTERVAL],
+            population_size=50,
+            max_generations=100,
+            mutation_probability=Decimal('0.2'),
+            verbose=True,
+        )
+
+        logging.info(
+            'training trading summary: '
+            f'{format_attrs_as_json(optimization_summary.trading_summary)}'
+        )
+        logging.info(
+            'training portfolio stats: '
+            f'{format_attrs_as_json(optimization_summary.portfolio_stats)}'
+        )
+
+        tc = optimization_summary.trading_config
+
+        # TODO: Ensure we can pass trading config directly as kwargs.
+        trading_summary = await trader.run(
+            exchange=tc.exchange,
+            symbol=tc.symbol,
+            interval=tc.interval,
+            start=validation_start,
+            end=validation_end,
+            quote=tc.quote,
+            new_strategy=lambda: optimization_summary.strategy_type(
+                **optimization_summary.strategy_config
+            ),
+            missed_candle_policy=tc.missed_candle_policy,
+            trailing_stop=tc.trailing_stop,
+        )
+
+        base_asset, quote_asset = unpack_symbol(SYMBOL)
+        fiat_daily_prices = await prices.map_fiat_daily_prices(
+            (base_asset, quote_asset), validation_start, validation_end
+        )
+        benchmark = analyse_benchmark(fiat_daily_prices[quote_asset])
+        portfolio = analyse_portfolio(benchmark.g_returns, fiat_daily_prices, trading_summary)
+
+        logging.info(f'trading summary: {format_attrs_as_json(trading_summary)}')
+        logging.info(f'benchmark stats: {format_attrs_as_json(benchmark.stats)}')
+        logging.info(f'portfolio stats: {format_attrs_as_json(portfolio.stats)}')
+
+    logging.info('done')
+
+
+asyncio.run(main())
