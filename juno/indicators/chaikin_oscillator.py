1e5be524a836b5f10647b81a528d0af60330ef06
Jaanus Varus <jaanusvarus@gmail.com>
2020-02-26 17:32:17 +0100

    Sync

--- juno/indicators/chaikin_oscillator.py
+++ juno/indicators/chaikin_oscillator.py
@@ -1,16 +1,24 @@
 from decimal import Decimal
 
+from .ema import Ema
+
 
 class ChaikinOscillator:
     def __init__(self, short_period: int, long_period: int) -> None:
         self.value = Decimal('0.0')
-        self._t = 0
-        self._t1 = 0
+        self._money_flow_volume = Decimal('0.0')
+        self._short_ema = Ema(short_period)
+        self._long_ema = Ema(long_period)
 
     @property
     def req_history(self) -> int:
         return 0
 
     def update(self, high: Decimal, low: Decimal, close: Decimal, volume: Decimal) -> None:
-        raise NotImplementedError()
-        self._t = min(self._t + 1, self._t1)
+        if high != low:
+            money_flow_multiplier = ((close - low) - (high - close)) / (high - low)
+            self._money_flow_volume += money_flow_multiplier * volume
+
+        self._short_ema.update(self._money_flow_volume)
+        self._long_ema.update(self._money_flow_volume)
+        self.value = self._short_ema.value - self._long_ema.value
