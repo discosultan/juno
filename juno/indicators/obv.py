f098d20ef8360e0188292b8ae959957faf6395d2
Jaanus Varus <jaanusvarus@gmail.com>
2020-03-12 17:38:10 +0100

    Sync

--- juno/indicators/obv.py
+++ juno/indicators/obv.py
@@ -1,23 +1,32 @@
 from decimal import Decimal
 
+from .ema import Ema
+
 
 # On-Balance Volume
 class Obv:
     value: Decimal = Decimal('0.0')
     ema: Decimal = Decimal('0.0')
+    _ema: Ema
     _last_price: Decimal = Decimal('0.0')
     _t: int = 0
     _t1: int = 0
+
+    def __init__(self) -> None:
+        self._ema = Ema(21)
 
     @property
     def req_history(self) -> int:
         return 0
 
-    def update(self, volume: Decimal, price: Decimal) -> None:
+    def update(self, price: Decimal, volume: Decimal) -> None:
         if price > self._last_price:
             self.value += volume
         elif price < self._last_price:
             self.value -= volume
+
+        self._ema.update(self.value)
+        self.ema = self._ema.value
 
         self._last_price = price
         self._t = min(self._t + 1, self._t1)
