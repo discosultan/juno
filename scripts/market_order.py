2f0652d555bd00ef6b67de5e40b3b31704811e48
Jaanus Varus <jaanusvarus@gmail.com>
2020-04-21 17:12:14 +0200

    Sync

--- scripts/market_order.py
+++ scripts/market_order.py
@@ -4,46 +4,61 @@ import sys
 from decimal import Decimal
 from typing import Optional
 
-from juno import Fill, Side, brokers
+from juno import Fill, Side, brokers, exchanges
 from juno.components import Informant, Orderbook, Wallet
 from juno.config import from_env, init_instance
-from juno.exchanges import Binance
 from juno.storages import SQLite
 from juno.utils import unpack_symbol
 
 MARKET_BROKER_TYPE = brokers.Market2
 SIDE = Side.BUY
-EXCHANGE = 'binance'
-SYMBOL = 'eth-btc'
-BASE_ASSET, QUOTE_ASSET = unpack_symbol(SYMBOL)
-QUOTE: Optional[Decimal] = Decimal('0.005')
+EXCHANGE_TYPE = exchanges.Coinbase
+SYMBOL = 'btc-eur'
+QUOTE: Optional[Decimal] = Decimal('10.0')
 # QUOTE = None
 # BASE: Optional[Decimal] = Decimal('0.2')
 BASE = None
 TEST = False
+
+# MARKET_BROKER_TYPE = brokers.Market2
+# SIDE = Side.BUY
+# EXCHANGE_TYPE = exchanges.Binance
+# SYMBOL = 'eth-btc'
+# QUOTE: Optional[Decimal] = Decimal('0.005')
+# # QUOTE = None
+# # BASE: Optional[Decimal] = Decimal('0.2')
+# BASE = None
+# TEST = False
 
 if len(sys.argv) > 1:
     SIDE = Side[sys.argv[1].upper()]
 
 
 async def main() -> None:
-    binance = init_instance(Binance, from_env())
-    exchanges = [binance]
+    base_asset, quote_asset = unpack_symbol(SYMBOL)
+    exchange = init_instance(EXCHANGE_TYPE, from_env())
+    exchange_name = EXCHANGE_TYPE.__name__.lower()
+    exchanges = [exchange]
     sqlite = SQLite()
     informant = Informant(storage=sqlite, exchanges=exchanges)
     orderbook = Orderbook(exchanges=exchanges, config={'symbol': SYMBOL})
     wallet = Wallet(exchanges=exchanges)
     market = MARKET_BROKER_TYPE(informant, orderbook, exchanges)
-    async with binance, informant, orderbook, wallet:
-        base = BASE if BASE is not None else wallet.get_balance(EXCHANGE, BASE_ASSET).available
-        quote = QUOTE if QUOTE is not None else wallet.get_balance(EXCHANGE, QUOTE_ASSET).available
-        logging.info(f'base: {base} {BASE_ASSET}; quote: {quote} {QUOTE_ASSET}')
+    async with exchange, informant, orderbook, wallet:
+        base = (
+            BASE if BASE is not None else wallet.get_balance(exchange_name, base_asset).available
+        )
+        quote = (
+            QUOTE if QUOTE is not None
+            else wallet.get_balance(exchange_name, quote_asset).available
+        )
+        logging.info(f'base: {base} {base_asset}; quote: {quote} {quote_asset}')
         if SIDE is Side.BUY:
             res = await market.buy_by_quote(
-                exchange=EXCHANGE, symbol=SYMBOL, quote=quote, test=TEST
+                exchange=exchange_name, symbol=SYMBOL, quote=quote, test=TEST
             )
         else:
-            res = await market.sell(exchange=EXCHANGE, symbol=SYMBOL, size=base, test=TEST)
+            res = await market.sell(exchange=exchange_name, symbol=SYMBOL, size=base, test=TEST)
 
         logging.info(res)
         logging.info(f'{SIDE.name} {SYMBOL}')
