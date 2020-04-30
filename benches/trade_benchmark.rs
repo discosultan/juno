f82264551af123809ec94b6cc1f4c91f5def44c1
discosultan <jaanusvarus@gmail.com>
2020-04-30 10:45:08 +0200

    Cargo fmt

--- benches/trade_benchmark.rs
+++ benches/trade_benchmark.rs
@@ -1,11 +1,9 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use juno_rs::{
-    BorrowInfo,
-    Candle,
-    Fees,
     filters::{Filters, Price, Size},
     strategies::MAMACX,
     trade::trade,
+    BorrowInfo, Candle, Fees,
 };
 
 const MIN_MS: u64 = 60000;
@@ -14,7 +12,7 @@ const MIN_MS: u64 = 60000;
 const EMA: u32 = 40698164;
 
 fn trade_benchmark(c: &mut Criterion) {
-    let strategy_factory = || { MAMACX::new(9, 24, -0.1, 0.1, 0, EMA, EMA) };
+    let strategy_factory = || MAMACX::new(9, 24, -0.1, 0.1, 0, EMA, EMA);
     let num_candles = 525600;
     let mut candles = Vec::with_capacity(num_candles);
     for i in 0..num_candles {
@@ -49,20 +47,24 @@ fn trade_benchmark(c: &mut Criterion) {
         daily_interest_rate: 0.001,
         limit: 1.0,
     };
-    c.bench_function("trade", |b| b.iter(|| trade(
-        strategy_factory,
-        &candles,
-        &fees,
-        &filters,
-        &borrow_info,
-        3,
-        60000,
-        1.0,
-        0,
-        0.0,
-        true,
-        true,
-    )));
+    c.bench_function("trade", |b| {
+        b.iter(|| {
+            trade(
+                strategy_factory,
+                &candles,
+                &fees,
+                &filters,
+                &borrow_info,
+                3,
+                60000,
+                1.0,
+                0,
+                0.0,
+                true,
+                true,
+            )
+        })
+    });
 }
 
 criterion_group!(benches, trade_benchmark);
