90b46581cca4f448df73f425168c75168114189b
Jaanus Varus <jaanusvarus@gmail.com>
2020-04-30 19:04:08 +0200

    Parameterize order scripts

--- scripts/exchange_cancel_order.py
+++ scripts/exchange_cancel_order.py
@@ -1,17 +1,23 @@
+import argparse
 import asyncio
 import logging
 
 from juno import exchanges
 from juno.config import from_env, init_instance
 
-EXCHANGE_TYPE = exchanges.Coinbase
+EXCHANGE_TYPE = exchanges.Binance
 CLIENT_ID = 'b311e925-6c5c-40ad-b73d-e79494af4d81'
-SYMBOL = 'btc-eur'
+SYMBOL = 'eth-btc'
+
+parser = argparse.ArgumentParser()
+parser.add_argument('symbol', nargs='?', default=SYMBOL)
+parser.add_argument('client_id', nargs='?', default=CLIENT_ID)
+args = parser.parse_args()
 
 
 async def main() -> None:
     async with init_instance(EXCHANGE_TYPE, from_env()) as client:
-        res = await client.cancel_order(symbol=SYMBOL, client_id=CLIENT_ID)
+        res = await client.cancel_order(symbol=args.symbol, client_id=args.client_id)
         logging.info(res)
 
 
