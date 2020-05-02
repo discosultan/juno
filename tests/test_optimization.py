98a2822cf6c67ea91add0bd501d890b5641006e1
discosultan <jaanusvarus@gmail.com>
2020-05-02 13:37:22 +0200

    Fix test

--- tests/test_optimization.py
+++ tests/test_optimization.py
@@ -1,8 +1,6 @@
 import math
 from decimal import Decimal
-from typing import AsyncIterator, List, Tuple
-
-import pytest
+from typing import List, Tuple
 
 from juno import Candle, Fees, Filters
 from juno.components import Prices
@@ -16,13 +14,7 @@ from juno.utils import load_json_file
 from . import fakes
 
 
-@pytest.fixture
-async def rust_solver(loop) -> AsyncIterator[Rust]:
-    async with Rust(informant=fakes.Informant()) as rust:
-        yield rust
-
-
-async def test_optimizer_same_result_with_predefined_seed(request, rust_solver: Rust) -> None:
+async def test_optimizer_same_result_with_predefined_seed(request) -> None:
     portfolio_candles = raw_to_type(
         load_json_file(__file__, './data/binance_eth-btc_3600000_candles.json'),
         List[Candle]
@@ -52,6 +44,7 @@ async def test_optimizer_same_result_with_predefined_seed(request, rust_solver: 
         fees=fees,
         filters=filters
     )
+    rust_solver = Rust(informant=informant)
     trader = Trader(chandler=chandler, informant=informant)
     optimizer = Optimizer(
         solver=rust_solver, chandler=chandler, informant=informant, prices=prices, trader=trader,
@@ -59,23 +52,25 @@ async def test_optimizer_same_result_with_predefined_seed(request, rust_solver: 
     )
 
     results = []
-    for _ in range(0, 2):
-        summary = await optimizer.run(
-            exchange='binance',
-            start=portfolio_candles[0].time,
-            end=portfolio_candles[-1].time + HOUR_MS,
-            strategy='mamacx',
-            quote=Decimal('1.0'),
-            population_size=5,
-            max_generations=10,
-            seed=1
-        )
-        results.append(summary.best[0].portfolio_stats)
+
+    async with rust_solver:
+        for _ in range(0, 2):
+            summary = await optimizer.run(
+                exchange='binance',
+                start=portfolio_candles[0].time,
+                end=portfolio_candles[-1].time + HOUR_MS,
+                strategy='mamacx',
+                quote=Decimal('1.0'),
+                population_size=5,
+                max_generations=10,
+                seed=1
+            )
+            results.append(summary.best[0].portfolio_stats)
 
     assert results[0].alpha == results[1].alpha
 
 
-async def test_rust_solver_works_with_default_fees_filters(rust_solver: Rust) -> None:
+async def test_rust_solver_works_with_default_fees_filters() -> None:
     portfolio_candles = raw_to_type(
         load_json_file(__file__, './data/binance_eth-btc_3600000_candles.json'),
         List[Candle]
@@ -95,24 +90,25 @@ async def test_rust_solver_works_with_default_fees_filters(rust_solver: Rust) ->
     benchmark_stats = analyse_benchmark(fiat_daily_candles['btc'])
     strategy_args = (11, 21, Decimal('-0.229'), Decimal('0.1'), 4, 'ema', 'ema')
 
-    result = rust_solver.solve(
-        Rust.Config(
-            fiat_daily_prices=fiat_daily_candles,
-            benchmark_g_returns=benchmark_stats.g_returns,
-            strategy_type=MAMACX,
-            start=portfolio_candles[0].time,
-            end=portfolio_candles[-1].time + HOUR_MS,
-            quote=Decimal('1.0'),
-            candles=portfolio_candles,
-            exchange='exchange',
-            symbol='eth-btc',
-            interval=HOUR_MS,
-            missed_candle_policy=MissedCandlePolicy.IGNORE,
-            trailing_stop=Decimal('0.0'),
-            long=True,
-            short=False,
-            strategy_args=strategy_args,
+    async with Rust(informant=fakes.Informant()) as rust_solver:
+        result = rust_solver.solve(
+            Rust.Config(
+                fiat_daily_prices=fiat_daily_candles,
+                benchmark_g_returns=benchmark_stats.g_returns,
+                strategy_type=MAMACX,
+                start=portfolio_candles[0].time,
+                end=portfolio_candles[-1].time + HOUR_MS,
+                quote=Decimal('1.0'),
+                candles=portfolio_candles,
+                exchange='exchange',
+                symbol='eth-btc',
+                interval=HOUR_MS,
+                missed_candle_policy=MissedCandlePolicy.IGNORE,
+                trailing_stop=Decimal('0.0'),
+                long=True,
+                short=False,
+                strategy_args=strategy_args,
+            )
         )
-    )
 
     assert not math.isnan(result.alpha)
