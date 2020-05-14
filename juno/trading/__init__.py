1f495f717dfbe1be04417aaa25eb47ec0b4508e6
discosultan <jaanusvarus@gmail.com>
2020-05-14 13:08:17 +0200

    Rename multitrader file

--- juno/trading/__init__.py
+++ juno/trading/__init__.py
@@ -1,6 +1,6 @@
 from .common import MissedCandlePolicy, Position, TradingSummary
 from .mixins import PositionMixin, SimulatedPositionMixin
-from .multitrader import MultiTrader
+from .multi_trader import MultiTrader
 from .statistics import AnalysisSummary, Statistics, analyse_benchmark, analyse_portfolio
 from .trader import Trader
 
