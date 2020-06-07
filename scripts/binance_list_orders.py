ecaf519f821a5e44f89449b7d48d6e0fe1bf2168
discosultan <jaanusvarus@gmail.com>
2020-06-07 12:51:33 +0200

    Update list_orders script

--- scripts/binance_list_orders.py
+++ scripts/binance_list_orders.py
@@ -5,17 +5,20 @@ import logging
 from juno.config import from_env, init_instance
 from juno.exchanges import Binance
 
-SYMBOL = 'eth-btc'
-MARGIN = False
-
 parser = argparse.ArgumentParser()
-parser.add_argument('symbol', nargs='?', default=SYMBOL)
+parser.add_argument('symbol', nargs='?', default='eth-btc')
+parser.add_argument(
+    '-m', '--margin',
+    action='store_true',
+    default=False,
+    help='if set, use margin; otherwise spot account',
+)
 args = parser.parse_args()
 
 
 async def main() -> None:
     async with init_instance(Binance, from_env()) as client:
-        orders = await client.list_orders(symbol=args.symbol, margin=MARGIN)
+        orders = await client.list_orders(symbol=args.symbol, margin=args.margin)
         logging.info(orders)
 
 asyncio.run(main())
