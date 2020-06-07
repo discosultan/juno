3fdf9b56ae419ed64e5e095444846c594059bab8
discosultan <jaanusvarus@gmail.com>
2020-06-07 12:09:08 +0200

    Support default value in config.resolve_concrete

--- juno/config.py
+++ juno/config.py
@@ -236,19 +236,25 @@ def type_to_config(value: Any, type_: Any) -> Any:
     return value
 
 
-def resolve_concrete(type_: Type[Any], config: Dict[str, Any]) -> Type[Any]:
+def resolve_concrete(
+    type_: Type[Any], config: Dict[str, Any], default: Any = inspect.Parameter.empty
+) -> Type[Any]:
     if not inspect.isabstract(type_):
         raise ValueError(f'Unable to resolve concrete type for a non-abstract type {type_}')
 
     abstract_name = type_.__name__.lower()
     concrete_name = config.get(abstract_name)
     if not concrete_name:
-        raise ValueError(f'Concrete name not found for {abstract_name} in config')
+        if default is inspect.Parameter.empty:
+            raise ValueError(f'Concrete name not found for {abstract_name} in config')
+        return default
 
     module_type_map = _map_type_parent_module_types(type_)
     concrete_type = module_type_map.get(concrete_name)
     if not concrete_type:
-        raise ValueError(f'Concrete type {concrete_name} not found')
+        if default is inspect.Parameter.empty:
+            raise ValueError(f'Concrete type {concrete_name} not found')
+        return default
 
     return concrete_type
 
