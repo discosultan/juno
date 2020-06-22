16b7f0a4b9653c5781e183fe5c2e193637e5b3d2
discosultan <jaanusvarus@gmail.com>
2020-06-22 19:11:07 +0200

    Sync

--- juno/plugins/plot.py
+++ juno/plugins/plot.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import logging
-from typing import Dict, Iterable
+from typing import Iterable, List
 
 import plotly.graph_objs as go
 import plotly.offline as py
@@ -21,23 +21,24 @@ class Discord(Plugin):
         self._events = events
 
     async def activate(self, agent_name: str, agent_type: str) -> None:
-        candles_map = {}
+        candles = []
 
         @self._events.on(agent_name, 'candle')
         async def on_candle(candle: Candle) -> None:
-            candles_map[candle.time] = candle
+            candles.append(candle)
 
         @self._events.on(agent_name, 'finished')
         async def on_finished(summary: TradingSummary) -> None:
-            plot(candles_map, summary.get_positions())
+            plot(candles, summary.get_positions())
 
         _log.info(f'activated for {agent_name} ({agent_type})')
 
 
-def plot(candles_map: Dict[int, Candle], positions: Iterable[Position.Closed]) -> None:
-    candles = candles_map.values()
+def plot(candles: List[Candle], positions: Iterable[Position.Closed]) -> None:
+    candles_map = {c.time: c for c in candles}
     times = [datetime_utcfromtimestamp_ms(c.time) for c in candles]
 
+    # Candles.
     trace1 = go.Ohlc(
         x=times,
         yaxis='y2',
@@ -46,29 +47,42 @@ def plot(candles_map: Dict[int, Candle], positions: Iterable[Position.Closed]) -
         low=[c.low for c in candles],
         close=[c.close for c in candles]
     )
+    # Volume.
+    trace2 = go.Bar(
+        x=times,
+        y=[c.volume for c in candles],
+        yaxis='y',
+        marker={
+            'color': ['#006400' if c.close >= c.open else '#8b0000' for c in candles]
+        }
+    )
+    # Openings.
     long_positions = [p for p in positions if isinstance(p, Position.Long)]
-    trace2 = go.Scatter(
+    trace3 = go.Scatter(
         x=[datetime_utcfromtimestamp_ms(p.open_time) for p in long_positions],
         y=[candles_map[p.open_time].close for p in long_positions],
         yaxis='y2',
         marker={
+            'symbol': 'triangle-up',
             'color': 'green',
             'size': 12,
         },
         mode='markers',
     )
     short_positions = [p for p in positions if isinstance(p, Position.Short)]
-    trace3 = go.Scatter(
+    trace4 = go.Scatter(
         x=[datetime_utcfromtimestamp_ms(p.open_time) for p in short_positions],
         y=[candles_map[p.open_time].close for p in short_positions],
         yaxis='y2',
         marker={
+            'symbol': 'triangle-down',
             'color': 'yellow',
             'size': 12,
         },
         mode='markers',
     )
-    trace4 = go.Scatter(
+    # Closings.
+    trace5 = go.Scatter(
         x=[datetime_utcfromtimestamp_ms(p.close_time) for p in positions],
         y=[candles_map[p.close_time].close for p in positions],
         yaxis='y2',
@@ -77,14 +91,6 @@ def plot(candles_map: Dict[int, Candle], positions: Iterable[Position.Closed]) -
             'size': 12,
         },
         mode='markers',
-    )
-    trace5 = go.Bar(
-        x=times,
-        y=[c.volume for c in candles],
-        yaxis='y',
-        marker={
-            'color': ['#006400' if c.close >= c.open else '#8b0000' for c in candles]
-        }
     )
     data = [trace1, trace2, trace3, trace4, trace5]
     layout = {
