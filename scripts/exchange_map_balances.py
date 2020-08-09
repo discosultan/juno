5da1c4b2d89209fcb6302677892aec4695a87a4d
Jaanus Varus <jaanusvarus@gmail.com>
2020-08-09 18:32:56 +0200

    Sync

--- scripts/exchange_map_balances.py
+++ scripts/exchange_map_balances.py
@@ -1,15 +1,31 @@
+import argparse
 import asyncio
 import logging
 
-from juno import exchanges
+from juno import AccountType, exchanges
 from juno.config import from_env, init_instance
+from juno.utils import get_module_type
 
-EXCHANGE_TYPE = exchanges.Binance
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    'account',
+    nargs='?',
+    type=lambda s: AccountType[s.upper()],
+    default=AccountType.SPOT,
+)
+# parser.add_argument('isolated_symbol', nargs='?', default=None)
+parser.add_argument(
+    '-e',
+    '--exchange',
+    type=lambda e: get_module_type(exchanges, e),
+    default=exchanges.Binance,
+)
+args = parser.parse_args()
 
 
 async def main() -> None:
-    async with init_instance(EXCHANGE_TYPE, from_env()) as client:
-        balances = await client.map_balances()
+    async with init_instance(args.exchange, from_env()) as client:
+        balances = await client.map_balances(margin=args.account is AccountType.CROSS_MARGIN)
         logging.info(balances)
 
 
