53635972eeb3896767623c1b2d397b0b3b388d1a
discosultan <jaanusvarus@gmail.com>
2020-09-06 19:27:15 +0200

    Cleanup

--- tests/test_wallet.py
+++ tests/test_wallet.py
@@ -1,28 +1,26 @@
 from decimal import Decimal
-from unittest.mock import AsyncMock
 
 from juno import Balance
 from juno.components import Wallet
 
-from . import fakes
 
-
-async def test_get_balance() -> None:
+async def test_get_balance(mocker) -> None:
     balance = Balance(available=Decimal('1.0'), hold=Decimal('0.0'))
-    exchange = fakes.Exchange(future_balances=[{'btc': balance}])
-    exchange.can_margin_trade = False
+
+    exchange = mocker.patch('juno.exchanges.Exchange', autospec=True)
+    exchange.map_balances.return_value = {'spot': {'btc': balance}}
 
     async with Wallet(exchanges=[exchange]) as wallet:
         out_balance = wallet.get_balance(
-            exchange='exchange', account='spot', asset='btc'
+            exchange='magicmock', account='spot', asset='btc'
         )
 
     assert out_balance == balance
 
 
-async def test_all_significant_balances(mocker) -> None:
+async def test_map_all_significant_balances(mocker) -> None:
     exchange = mocker.patch('juno.exchanges.Exchange', autospec=True)
-    exchange.map_balances = AsyncMock(side_effect=[
+    exchange.map_balances.side_effect = [
         {
             'spot': {
                 'eth': Balance(),
@@ -35,7 +33,7 @@ async def test_all_significant_balances(mocker) -> None:
                 'btc': Balance(),
             },
         }
-    ])
+    ]
 
     async with Wallet(exchanges=[exchange]) as wallet:
         balances = await wallet.map_balances(
