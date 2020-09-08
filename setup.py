2c734c3f984a130877d51c8c891aff41f8c84707
Jaanus Varus <jaanusvarus@gmail.com>
2020-09-08 10:41:56 +0200

    Add python_requires to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.5.0',
     packages=find_packages(),
     include_package_data=True,
+    python_requires='>= 3.8',
     install_requires=[
         'aiohttp',
         'aiolimiter',
