9c751423e08678a22122c175d72a137e1bef0193
discosultan <jaanusvarus@gmail.com>
2020-10-12 23:56:43 +0200

    Cleanup

--- juno/strategies/fixed.py
+++ juno/strategies/fixed.py
@@ -13,8 +13,11 @@ class Fixed:
     advices: List[Advice]
     updates: List[Candle]
     cancel: bool
-    mid_trend: MidTrend
-    persistence: Persistence
+
+    _maturity: int
+    _mid_trend: MidTrend
+    _persistence: Persistence
+    _t: int = 0
 
     def __init__(
         self,
@@ -24,16 +27,25 @@ class Fixed:
         persistence: int = 0,
         cancel: bool = False,
     ) -> None:
-        super().__init__(
-            maturity=maturity,
-            persistence=persistence,
-            mid_trend_policy=mid_trend_policy,
-        )
         self.advices = list(advices)
         self.updates = []
         self.cancel = cancel
 
+        self._maturity = maturity
+        self._mid_trend = MidTrend(mid_trend_policy)
+        self._persistence = Persistence(level=persistence, return_previous=False)
+
+    @property
+    def maturity(self) -> int:
+        return self._maturity
+
+    @property
+    def mature(self) -> bool:
+        return self._t >= self._maturity
+
     def tick(self, candle: Candle) -> Advice:
+        self._t = min(self._t + 1, self._maturity)
+
         self.updates.append(candle)
         if len(self.advices) > 0:
             return self.advices.pop(0)
