740fbc21bb7b82413d0a0b51cdbda19ca3a2d97b
discosultan <jaanusvarus@gmail.com>
2022-05-13 12:07:50 +0200

    Add bmsb trader

--- juno/traders/__init__.py
+++ juno/traders/__init__.py
@@ -1,5 +1,6 @@
 from .basic import Basic, BasicConfig, BasicState
 from .bbands import BBandsConfig, BBandsState, BBandsTrader
+from .bmsb import BMSBConfig, BMSBState, BMSBTrader
 from .multi import Multi, MultiConfig, MultiState
 from .trader import Trader
 
@@ -10,6 +11,9 @@ __all__ = [
     "BBandsConfig",
     "BBandsState",
     "BBandsTrader",
+    "BMSBConfig",
+    "BMSBState",
+    "BMSBTrader",
     "Multi",
     "MultiConfig",
     "MultiState",
